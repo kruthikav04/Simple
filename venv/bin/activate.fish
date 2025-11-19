4649f8390c1cfd23df4ea6188b4dafdf88b5c9c9
kruthika <kruthika@airowire.com>
2025-11-19 10:37:40 +0000

    created simple app

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/backup_gcp/simple-app/venv
+set -gx VIRTUAL_ENV /home/backup_gcp/simple/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
